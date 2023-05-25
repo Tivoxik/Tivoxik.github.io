a71ea88544e5a8138a72e7ad9fbbc61fbf5edf3b
Tivoxik <tivoxpl12@gmail.com>
2023-05-25 19:19:12 +0200

    Added to github pages
    -

---
+++ loader.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+    setTimeout(function(){
+    //   var loader = document.getElementById("loader");
+    //   var content = document.getElementById("content");
+  
+      container.style.display = "none";
+      content.style.display = "flex";
+    }, 3000);
+  });
+  
---
+++ main.js
@@ -0,0 +1,29 @@
+function dane() {  
+
+    const imie = document.getElementById('imie').value;
+    const nazwisko = document.getElementById('nazwisko').value;
+    const email = document.getElementById('email').value;
+    const adres = document.getElementById('adres').value;
+
+    let dane = document.getElementById('dane').innerHTML = "Twoje dane do wysyłki to: " +imie+" "+nazwisko+", "+email+", "+adres
+
+};
+
+function produkty() {
+
+    const samyang = parseInt(document.getElementById("sam").value);
+    const samcena = 1790
+    const cenasam = (samyang*samcena)
+    const zeis = parseInt(document.getElementById("zeis").value);
+    const zeiscena = 18800
+    const cenazeis = (zeis*zeiscena)
+    const sony = parseInt(document.getElementById("sony").value);
+    const sonycena = 7940
+    const cenasony = (zeis*sonycena)
+    const canon = parseInt(document.getElementById("canon").value);
+    const canoncena = 32800
+    const cenacanon = (zeis*canoncena)
+
+    let produkty = document.getElementById('stuff').innerHTML = "Twoje produty to: " + "Samyang sztuk " +samyang+" o łącznej wartości: "+cenasam+", Zeis sztuk " +zeis+" o łącznej wartości: "+cenazeis+", Sony sztuk " +sony+" o łącznej wartości: "+cenasony+", Canon sztuk " +canon+" o łącznej wartości: "+cenacanon
+
+};
